3e272140c47d2715f3b6c1575937aad39e3205fa
YoussefKharrata <kharrata.youssef2004@gmail.com>
2026-02-18 20:37:44 +0000

    Fixing localhost binding to allow external access to the Flask app.

--- app/app.py
+++ app/app.py
@@ -99,6 +99,6 @@ def logout():
 
 if __name__ == "__main__":
     debug_mode = os.environ.get("FLASK_DEBUG", "False").lower() in ("true", "1", "yes")
-    host = os.environ.get("FLASK_HOST", "127.0.0.1")
+    host = os.environ.get("FLASK_HOST", "0.0.0.0")
     port = int(os.environ.get("FLASK_PORT", 5000))
     app.run(host=host, port=port, debug=debug_mode)
